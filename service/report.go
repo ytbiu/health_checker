e9a9451f1b2cad0efd38e09d9cf05bf544e22a54
ytbiu <174920143@qq.com>
2024-07-09 18:36:40 +0800

    minor

--- service/report.go
+++ service/report.go
@@ -1,7 +1,7 @@
 package service
 
 import (
-	"github.com/sirupsen/logrus"
+	"health_checker/config"
 	"health_checker/service/common"
 )
 
@@ -9,30 +9,15 @@ type RegisterModel struct {
 	Model string
 }
 
-func Report(nodeId, project string, model []RegisterModel) error {
+func Report(nodeId, project string, models []RegisterModel) error {
 	fetchInfo := common.GetNvidiaFetchInfo()
-	logrus.Infof("%v", map[string]interface{}{
+	return common.Post(config.ConfigInfo.DBCHealthCheckReportUrl(), map[string]interface{}{
 		"node_id":        nodeId,
 		"project":        project,
-		"models":         model,
+		"models":         models,
 		"GPUName":        fetchInfo.GPUName,
 		"UtilizationGPU": fetchInfo.UtilizationGPU,
 		"MemoryTotal":    fetchInfo.MemoryTotal,
 		"MemoryUsed":     fetchInfo.MemoryUsed,
 	})
-
-	//err := common.Post(config.ConfigInfo.DBCHealthCheckReportUrl(), map[string]interface{}{
-	//	"node_id":        nodeId,
-	//	"project_name":   projectName,
-	//	"model":          model,
-	//	"GPUName":        fetchInfo.GPUName,
-	//	"UtilizationGPU": fetchInfo.UtilizationGPU,
-	//	"MemoryTotal":    fetchInfo.MemoryTotal,
-	//	"MemoryUsed":     fetchInfo.MemoryUsed,
-	//})
-	//if err != nil {
-	//	return err
-	//}
-
-	return nil
 }
